eb9c779d12235c7456f62aa39c14446693563d0f
ramya-k-murthy <ramyakmurthy1119@gmail.com>
2023-03-30 17:11:21 +0530

    folders renamed

--- adapter/VSK_data_transformation.sh
+++ adapter/VSK_data_transformation.sh
@@ -4,14 +4,14 @@ python DIKSHA_etb_plays_per_capita.py diksha diksha_etb_plays-per-capita.zip
 python DIKSHA_etb_qr_coverage.py diksha vsk_diksha_etb_qr-coverage.zip
 python NISHTHA_consumption.py nishtha diksha_nishtha_consumption-by-district.zip
 python NISHTHA_consumption_by_courses.py nishtha diksha_nishtha_consumption-by-course.zip
-python NISHTHA_percentage_enrollment_certification.py NISHTHA diksha_nishtha_percentage-enrollment-certification.zip
+python NISHTHA_percentage_enrollment_certification.py nishtha diksha_nishtha_percentage-enrollment-certification.zip
 python NISHTHA_program_started.py nishtha diksha_nishtha_program-started.zip
 python NISHTHA_tot_courses_medium.py nishtha diksha_nishtha_tot-courses-medium.zip
 python NAS_all_dashboard.py nas nas_all-dashboard.zip
 python NAS_program_started.py nas nas_program-started.zip
 python PGI_all_dashboard.py pgi pgi_all-dashboard.zip
 python PGI_performance.py pgi  pgi_state-wise-performance.zip
-python PM_Poshan.py pm_poshan pm-poshan_access-across-india.zip
+python PM_Poshan.py pm-poshan pm-poshan_access-across-india.zip
 python UDISE_all_dashboard.py udise udise-all-dashboard.zip
 python UDISE_program_started.py udise udise_program-started.zip
 
