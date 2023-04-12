e66a5dfdf6a55624fcdefb8b24054cae2b3ffca0
ramya-k-murthy <ramyakmurthy1119@gmail.com>
2023-04-12 13:15:32 +0530

    renamed column in PM-poshan

--- adapter/PM_Poshan.py
+++ adapter/PM_Poshan.py
@@ -5,13 +5,13 @@ program=obj.program
 df_data = obj.get_file()
 
 def total_meal_served():
-    df_snap = df_data[['District Code', 'MealServed(02/July/2022)']]
+    df_snap = df_data[['District Code', 'MealServed']]
     df_snap.columns = ['district_id', 'total_meals_served']
     obj.upload_file(df_snap, 'totalmealserved-event.data.csv')
 
 def category_event_data():
     df_melt=df_data.melt(id_vars=['District Code'],
-                     value_vars=['Enrolled In July','Total Schools'],
+                     value_vars=['Enrolled','Total Schools'],
                      var_name="category_name",value_name="category_value")
     df_snap=df_melt[['District Code','category_name','category_value']]
     df_snap.columns=['district_id','category_name','category_value']
