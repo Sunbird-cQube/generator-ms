45ff41dbdc51362fa4022af991566a750c60089c
ramya-k-murthy <ramyakmurthy1119@gmail.com>
2023-04-12 13:16:10 +0530

    renamed column in PM-poshan

--- adapter/NVSK/MICRO_Improvement_all_dashboard.py
+++ adapter/NVSK/MICRO_Improvement_all_dashboard.py
@@ -9,7 +9,7 @@ df_data=obj.get_file()
 def participating_program_status():
     df_snap = df_data[['State Code', 'Started Micro-Improvement']]
     df_snap.columns = ['state_id', 'participating_in_micro_improvement_program_status']
-    obj.upload_file(df_snap, 'participatingprogramstatus-event.data.csv')
+    obj.upload_file(df_snap, 'programstartedmicro-event.data.csv')
 
 if df_data is not None:
     participating_program_status()
