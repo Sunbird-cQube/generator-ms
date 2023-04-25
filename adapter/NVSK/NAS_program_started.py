3fb9869c9489202bcd73f48ec35300e32c847990
ramya-k-murthy <ramyakmurthy1119@gmail.com>
2023-04-25 19:48:12 +0530

    updated adapter

--- adapter/NVSK/NAS_program_started.py
+++ adapter/NVSK/NAS_program_started.py
@@ -7,7 +7,7 @@ df_data=obj.get_file()
 def program_status():
     df_snap = df_data[['State Code','Started']].drop_duplicates()
     df_snap.columns = ['state_id','started']
-    obj.upload_file(df_snap, 'programstartednas-event.data.csv')
+    obj.upload_file(df_snap, 'programstarted-event.data.csv')
 
 if df_data is not None:
     program_status()
