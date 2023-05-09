6eb6e2343c4949f0a05765a86a9d40a0a65b3534
ramya-k-murthy <ramyakmurthy1119@gmail.com>
2023-05-09 11:16:19 +0530

    changed adapter names according to UI dashboard

--- adapter/NVSK/NVSK_data_transformation.sh
+++ adapter/NVSK/NVSK_data_transformation.sh
@@ -1,30 +1,40 @@
-python3 PM_Poshan_progress_status.py pm_poshan pm-poshan_access-across-india.csv
-python3 PM_Poshan_implementation_status.py pm_poshan pm-poshan_state-onboarded.csv
-python3 PGI_implementation_status.py pgi pgi_program-started.csv
-python3 PGI_district_wise_performance.py pgi pgi_all-dashboard.csv
-python3 PGI_state_wise_performance.py pgi pgi_state-wise-performance.csv
-python3 UDISE_state_wise_performance.py udise udise-all-dashboard.csv
-python3 UDISE_implementation_status.py udise udise_program-started.csv
-python3 NAS_state_wise_performance.py nas nas_all-dashboard.csv
-python3 NAS_implementation_status.py nas nas_program-started.csv
-python3 NISHTHA_district_wise_status.py nishtha diksha_nishtha_consumption-by-district.csv
-python3 NISHTHA_course_wise_status.py nishtha diksha_nishtha_consumption-by-course.csv
-python3 NISHTHA_percentage_against_potential_base.py nishtha diksha_nishtha_percentage-enrollment-certification.csv
-python3 NISHTHA_implementation_status.py nishtha diksha_nishtha_program-started.csv
-python3 NISHTHA_courses_medium_status.py nishtha diksha_nishtha_tot-courses-medium.csv
-python3 DIKSHA_etb_coverage.py diksha diksha_etb_coverage-status.csv
-python3 DIKSHA_learning_session.py diksha diksha_etb_learning-session.csv
-python3 DIKSHA_etb_plays_per_capita.py diksha diksha_etb_plays-per-capita.csv
 python3 DIKSHA_content_coverage_on_qr.py diksha diksha_etb_qr-coverage.csv
-python3 DIKSHA_implementation_status.py diksha diksha_etb_program-started.csv
 python3 DIKSHA_etb_coverage_status.py diksha diksha_etb_etb-coverage.csv
+python3 DIKSHA_implementation_status.py diksha diksha_etb_program-started.csv
+python3 DIKSHA_learning_session.py diksha diksha_etb_learning-session.csv
+
 python3 MICRO_implementation_status.py micro_improvement micro-improvement_district.csv
 python3 MICRO_Improvement_status.py micro_improvement micro-improvement_all-dashboard.csv
+
+python3 NAS_implementation_status.py nas nas_program-started.csv
+python3 NAS_state_wise_performance.py nas nas_all-dashboard.csv
+
 python3 NCERT_implementation_status.py ncert diksha_quiz_program-started.csv
 python3 NCERT_participation_status.py ncert diksha_quiz_state-participation.csv
 python3 NCERT_quiz_wise_status.py ncert diksha_quiz-started-plan.csv
+
 python3 NCF_progress_status.py ncf ncf_all-dashboard.csv
 python3 NCF_DiSanc_survey.py ncf language_wise_count.csv
 python3 NCF_state_wise.py ncf state_wise_count.csv
-python3 NIPUN_textbook_status.py nipun_barath nipunbharat_lo-covered-by-textbook.csv
+
 python3 NIPUN_learning_session.py nipun_barath nipunbharat_content-consumption.csv
+python3 NIPUN_textbook_status.py nipun_barath nipunbharat_lo-covered-by-textbook.csv
+
+python3 NISHTHA_course_wise_status.py nishtha diksha_nishtha_consumption-by-course.csv
+python3 NISHTHA_courses_medium_status.py nishtha diksha_nishtha_tot-courses-medium.csv
+python3 NISHTHA_district_wise_status.py nishtha diksha_nishtha_consumption-by-district.csv
+python3 NISHTHA_implementation_status.py nishtha diksha_nishtha_program-started.csv
+python3 NISHTHA_percentage_against_potential_base.py nishtha diksha_nishtha_percentage-enrollment-certification.csv
+
+python3 PGI_district_wise_performance.py pgi pgi_all-dashboard.csv
+python3 PGI_state_wise_performance.py pgi pgi_state-wise-performance.csv
+python3 PGI_implementation_status.py pgi pgi_program-started.csv
+
+python3 PM_Poshan_implementation_status.py pm_poshan pm-poshan_state-onboarded.csv
+python3 PM_Poshan_progress_status.py pm_poshan pm-poshan_access-across-india.csv
+
+python3 UDISE_implementation_status.py udise udise_program-started.csv
+python3 UDISE_state_wise_performance.py udise udise-all-dashboard.csv
+
+
+
