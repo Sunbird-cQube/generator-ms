d4d30458d6261635277dbbe825013353953742f0
ramya-k-murthy <ramyakmurthy1119@gmail.com>
2023-05-12 13:06:50 +0530

    changed adapter names according to UI dashboard

--- adapter/NVSK/NAS_state_wise_performance.py
+++ adapter/NVSK/NAS_state_wise_performance.py
@@ -20,12 +20,12 @@ def student_event_data():
     obj.upload_file(df_snap, 'studentssurveyed-event.data.csv')
 
 def performance_event_data():
-    df_snap = df_data[['State Code', 'District Code', 'Grade', 'Subject', 'Indicator Code', 'Performance']]
+    df_snap = df_data[['State Code', 'District Code', 'Grade', 'Subject', 'Learning Outcome Code', 'Performance']]
     df_snap.columns = ['state_id', 'district_id', 'grade', 'subject', 'lo_code', 'performance']
     obj.upload_file(df_snap, 'performance-event.data.csv')
 
 def learning_outcome_dimension_data():
-    df_snap = df_data[['Indicator', 'Indicator Code']].drop_duplicates()
+    df_snap = df_data[['Learning Outcome', 'Learning Outcome Code']].drop_duplicates()
     df_snap.columns = ['lo_code', 'lo_name']
     obj.upload_file(df_snap, 'lo-dimension.data.csv')
 
