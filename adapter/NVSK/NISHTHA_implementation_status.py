ff0473de0f3ce4e9d092e78f6aea40c8c785b4dc
dhanush-2397 <dhanush.ks@tibilsolutions.com>
2023-07-27 17:36:28 +0530

    adapter changes for nishtha

--- adapter/NVSK/NISHTHA_implementation_status.py
+++ adapter/NVSK/NISHTHA_implementation_status.py
@@ -5,10 +5,27 @@ program=obj.program
 df_data=obj.get_file()
 
 def started_event_data():
-    df_snap = df_data[['state_code','program','started']]
-    df_snap.columns = ['state_id','program_name','started']
+    df_snap = df_data[['state_code','program_name','program_status','started']]
+    df_snap.columns = ['state_id','program_name','program_status','started']
     obj.upload_file(df_snap, 'programstarted-event.data.csv')
+
+def program_dimension_data():
+    df_snap = df_data[['program_name']].drop_duplicates()
+    df_snap['program_id'] = range(1, len(df_snap) + 1)
+    df_snap = df_snap[['program_id', 'program_name']]
+    df_snap.update(df_snap[['program_id']].applymap("'{}'".format))
+    obj.upload_file(df_snap, 'programnishtha-dimension.data.csv')
+
+def program_status_dimension_data():
+    df_snap = df_data[['program_status']].drop_duplicates()
+    df_snap['program_status_id'] = range(1, len(df_snap) + 1)
+    df_snap = df_snap[['program_status_id', 'program_status']]
+    df_snap.update(df_snap[['program_status_id']].applymap("'{}'".format))
+    obj.upload_file(df_snap, 'programstatusnishtha-dimension.data.csv')
 
 if df_data is not None:
     started_event_data()
+    program_dimension_data()
+    program_status_dimension_data()
+
 
--- adapter/NVSK/NISHTHA_percentage_against_potential_base.py
+++ adapter/NVSK/NISHTHA_percentage_against_potential_base.py
@@ -4,43 +4,11 @@ obj=CollectData()
 program=obj.program
 df_data=obj.get_file()
 
-def actual_enrolment():
-    df_snap = df_data[['state_code','program','total_enrolments']]
-    df_snap.columns = ['state_id','program_name','actual_enrolment']
-    obj.upload_file(df_snap, 'actualenrolment-event.data.csv')
-
-def actual_certification():
-    df_snap = df_data[['state_code', 'program','total_certificates_issued']]
-    df_snap.columns = ['state_id','program_name','actual_certification']
-    obj.upload_file(df_snap, 'actualcertification-event.data.csv')
-
-
-def target_achieved_enrolment():
-    df_snap = df_data[['state_code','program','%_target_achieved_enrolment']]
-    df_snap.columns = ['state_id','program_name','perc_target_achieved_enrolment']
-    obj.upload_file(df_snap, 'targetachievedenrolment-event.data.csv')
-
-def target_achieved_certificates():
-    df_snap = df_data[['state_code', 'program','%_target_achieved_certificates']]
-    df_snap.columns = ['state_id','program_name', 'perc_target_achieved_certificates']
-    obj.upload_file(df_snap, 'targetachievedcertificates-event.data.csv')
-
-def expected_enrolment():
-    df_snap = df_data[['state_code','program','total_expected_enrolment']]
-    df_snap.columns = ['state_id','program_name','expected_enrolment']
-    obj.upload_file(df_snap, 'expectedenrolment-event.data.csv')
-
-def expected_certification():
-    df_snap = df_data[['state_code', 'program','total_expected_certification']]
-    df_snap.columns = ['state_id','program_name','total_expected_certificates']
-    obj.upload_file(df_snap, 'expectedcertificates-event.data.csv')
+def total_participants():
+    df_snap = df_data[['program_name','total_participants']]
+    obj.upload_file(df_snap,'totalparticipants-event.data.csv')
 
 if df_data is not None:
-    actual_certification()
-    actual_enrolment()
-    target_achieved_enrolment()
-    target_achieved_certificates()
-    expected_enrolment()
-    expected_certification()
+    total_certificates_achieved()
 
 
